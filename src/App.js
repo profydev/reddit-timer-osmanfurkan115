323e06cefb021ba0c6b6576eaa28e71dea98ee7f
Osman Furkan <osmanfurkan115@gmail.com>
2022-01-10 20:49:49 +0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
   const [click, setClick] = useState(0);
@@ -6,6 +7,7 @@ function App() {
     <div>
       <h1>{click}</h1>
       <button onClick={() => setClick(click + 1)} type="button"> Click</button>
+      <Header></Header>
     </div>
   );
 }
